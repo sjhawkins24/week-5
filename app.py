e6c7334f9f30e9e05b6dade0ea04e79d55d5466c
Serena Hawkins <sjhawkins@ap.org>
2025-09-27 19:43:11 -0400

    Updates to the app file

--- app.py
+++ app.py
@@ -6,8 +6,10 @@ from apputil import *
 df = pd.read_csv('https://raw.githubusercontent.com/leontoddjohnson/datasets/main/data/titanic.csv')
 
 st.write(
-'''
-# Titanic Visualization 1
+'''The cliche with the Titanic is the saying 'Women and children' first.  \
+This would make you think that women and children would have a significantly higher survival \
+rate than men. But this was also a time with stark socioeconomic divides, so the question is: \
+Do women and children have higher survival rates across all passenger classes?"
 
 '''
 )
@@ -17,7 +19,11 @@ st.plotly_chart(fig1, use_container_width=True)
 
 st.write(
 '''
-# Titanic Visualization 2
+I would like to know if there is a relationship between class and family size. On one hand, I \
+         could see first class passangers having more family members on board because they inherently come \
+         from families with more weatlth. On the other hand, I could see third class passangers having \
+         more family memebers on board because if you are taking a big family, you probably want the least \
+         expensive ticket. 
 '''
 )
 # Generate and display the figure
@@ -30,5 +36,5 @@ st.write(
 '''
 )
 # Generate and display the figure
-fig3 = visualize_family_size()
-st.plotly_chart(fig3, use_container_width=True)
+#fig3 = visualize_family_size()
+#st.plotly_chart(fig3, use_container_width=True)
